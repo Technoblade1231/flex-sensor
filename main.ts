9d62e09e397cd2ca5ea91cd428a5dde3fdfaa14d
Technoblade1231 <92799244+Technoblade1231@users.noreply.github.com>
2021-11-10 09:34:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let Flex_Sensor = 0
 basic.forever(function () {
-	
+    Flex_Sensor = pins.analogReadPin(AnalogPin.P2)
+    basic.showNumber(Flex_Sensor)
+    if (Flex_Sensor > 0) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
